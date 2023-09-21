3395a5459ee218cf6878963712fb15244d264c33
ed system on behalf of Chris Polak <ed>
2023-09-21 14:34:43 +1000

    Scaffold update by instructor

---
+++ launcher.py
@@ -0,0 +1,15 @@
+"""
+Write code for your launcher here.
+
+You may import library modules allowed by the specs, as well as your own other modules.
+"""
+import sys
+
+
+def main(args: list[str]) -> None:
+    # TODO
+    pass
+
+
+if __name__ == "__main__":
+    main()

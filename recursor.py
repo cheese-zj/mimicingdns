fb0dc1e6d8d04ea6d9de800d268a86157004f5f2
James Zhao <jzha0385@uni.sydney.edu.au>
2023-10-17 00:17:59 +1100

    a

--- recursor.py
+++ recursor.py
@@ -1,8 +1,4 @@
-"""
-Write code for your recursor here.
 
-You may import library modules allowed by the specs, as well as your own other modules.
-"""
 from sys import argv
 
 import sys
@@ -29,25 +25,20 @@ def query_server(port, message, timeout):
 def resolve_domain(root_port, domain, timeout):
     start_time = time.time()
     try:
-        # Step 1
         port = root_port
         message = domain.split('.')[-1] + '\n'
         tld_port = int(query_server(port, message, timeout))
 
-        # Step 2 and 3
         domain_parts = domain.split('.')
         message = '.'.join(domain_parts[-2:]) + '\n'
         auth_port = int(query_server(tld_port, message, timeout))
 
-        # Step 4, 5 and 6
         final_port = int(query_server(auth_port, domain + '\n', timeout))
         return str(final_port) + '\n'
 
     except (socket.timeout, ValueError):
         elapsed_time = time.time() - start_time
-        return "NXDOMAIN"
-        # else:
-        #     return "ERROR"
+        return "NXDOMAIN\n"
 
 
 def main(args: list[str]) -> None:

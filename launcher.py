a4c9b6877b13cfa49b8d0f22a837e2e353f926ac
James Zhao <jzha0385@uni.sydney.edu.au>
2023-10-18 18:26:49 +1100

    a

--- launcher.py
+++ launcher.py
@@ -7,6 +7,7 @@ from sys import argv
 from recursor import is_valid_domain
 from pathlib import Path
 
+
 def main(args: list[str]) -> None:
     if len(args) != 2:
         print("INVALID ARGUMENTS")
@@ -25,6 +26,25 @@ def main(args: list[str]) -> None:
             for domain in master_data[1:]:
                 if not is_valid_domain(domain):
                     raise Exception
+        master_data = master_data[1:]
+        roots = set()
+        tld = dict()
+        auth = dict()
+
+        reflect = []
+        port_pointer = 1024
+
+        for line in master_data:
+            reflect = line.split(",")
+            auth[reflect[0]]=reflect[1]
+            roots.add(reflect[0].split(".")[-1])  # add roots eg "com" into roots set
+        with open(dir_of_single_files + "/root.conf", "w") as root_f:
+            root_f.write(str(port_pointer)+"\n")
+            port_pointer += 1
+            for root in roots:
+                root_f.write(root+","+str(port_pointer)+"\n")
+                port_pointer += 1
+
 
     except (Exception, ValueError):
         print("INVALID MASTER")

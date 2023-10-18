bbf766ad3eb9dfb9edcebd15dc1ef1c901fb168c
James Zhao <jzha0385@uni.sydney.edu.au>
2023-10-18 21:37:21 +1100

    ?

--- launcher.py
+++ launcher.py
@@ -13,8 +13,8 @@ def main(args: list[str]) -> None:
         print("INVALID ARGUMENTS")
         exit()
     master_file = str(args[0])
-    dir_of_single_files = Path(str(args[1]))
-    if not dir_of_single_files.is_dir():
+    dir_of_single_files = str(args[1])
+    if Path(dir_of_single_files).is_dir() is False:
         print("NON-WRITABLE SINGLE DIR")
         exit(1)
     try:
@@ -23,27 +23,64 @@ def main(args: list[str]) -> None:
             master_port = int(master_data[0])
             if not 1024 <= master_port <= 65535:
                 raise Exception
-            for domain in master_data[1:]:
+            master_data = master_data[1:]
+            for line in master_data:
+                domain = line.split(",")[0]
+                #print(domain)
                 if not is_valid_domain(domain):
                     raise Exception
-        master_data = master_data[1:]
+                port = int(line.split(",")[1])
+                #print(port)
+                if not 1024 <= port <= 65535:
+                    raise Exception
+            print("config check done")
+
         roots = set()
-        tld = dict()
         auth = dict()
 
-        reflect = []
-        port_pointer = 1024
+        port_pointer = master_port
 
         for line in master_data:
             reflect = line.split(",")
-            auth[reflect[0]]=reflect[1]
+            auth[reflect[0]]=int(reflect[1].strip())
             roots.add(reflect[0].split(".")[-1])  # add roots eg "com" into roots set
-        with open(dir_of_single_files + "/root.conf", "w") as root_f:
+        with open(dir_of_single_files+"/root.conf", "w") as root_f:
             root_f.write(str(port_pointer)+"\n")
             port_pointer += 1
             for root in roots:
                 root_f.write(root+","+str(port_pointer)+"\n")
                 port_pointer += 1
+
+        with open(dir_of_single_files + "/root.conf", "r") as root_f:
+            data = root_f.readlines()[1:]
+            for line in data:
+                line = line.split(",")
+                with open(dir_of_single_files + "/tld-" + line[0] + ".conf", "w") as tld_f:
+                    tld_f.write(line[1].strip()+"\n")
+                    for item in auth:
+                        if item.split(".")[-1] == line[0]:
+                            parts = item.split(".")
+                            domain = f"{parts[-2]}.{parts[-1]}"
+                            tld_f.write(f"{domain},{port_pointer}\n")
+                            port_pointer += 1
+
+        search_dir = Path(dir_of_single_files)
+        # Find all files that start with 'tld-'
+        matching_files = list(search_dir.glob("tld-*"))
+        for file in matching_files:
+            print(file)
+            with open(file, "r") as tld_f:
+                data = tld_f.readlines()[1:]
+                for line in data:
+                    line = line.split(",")
+                    f_dir = dir_of_single_files + "/auth-" + line[0].split(".")[0] + ".conf"
+                    with open(f_dir, "w") as auth_f:
+                        auth_f.write(line[1].strip()+"\n")
+                        for item in auth:
+                            if item.split(".")[-2]+"."+item.split(".")[-1] == line[0]:
+                                auth_f.write(f"{item},{auth[item]}\n")
+
+
 
 
     except (Exception, ValueError):
